2e7c6c9a39ffd0043d6801d1ad45ea33d351163a
Philipp Benz <phibenz@gmail.com>
2021-10-30 15:17:11 +0900

    Update docker

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,9 +1,8 @@
 docker run \
     -it --rm \
     --runtime=nvidia \
-    -p 8888:8888 \
+    -p 9999:9999 \
     --shm-size=32G \
     --volume /home/philipp/data_local:/workspace/data_local \
     --volume /home/philipp/Projects/adversarial_ml.research:/workspace/Projects/adversarial_ml.research \
-    --volume /media/philipp/ssd2_4tb:/workspace/ssd2_4tb \
     phibenz/adversarial_ml.research
